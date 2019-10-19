f697dcee2ca6caa8c40542679f9c3c54ced40b9e
BrianW <brianw.area24@gmail.com>
2019-10-19 11:47:42 -0500

    Added an exclude_from_feeds checkbox to the edit meetings form, and saved the results in the database

--- includes/save.php
+++ includes/save.php
@@ -82,6 +82,15 @@ function tsml_save_post($post_id, $post, $update) {
 		}
 	}
 
+	// exclude_from_feeds is a checkbox
+        if (empty($_POST['exclude_from_feeds'])) {
+                delete_post_meta($post->ID, 'exclude_from_feeds');
+        } else {
+                //$time_temp = $old_meeting->time;
+                update_post_meta($post->ID, 'exclude_from_feeds', $_POST['exclude_from_feeds']);
+        }
+
+
 	//day could be null for appointment meeting
 	if (in_array($_POST['day'], array('0', '1', '2', '3', '4', '5', '6'))) {
 		if (!$update || !isset($old_meeting->day) || $old_meeting->day != intval($_POST['day'])) {
